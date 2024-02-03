4807b6fa912cd144df00f63e702f79e43335cfd9
@ekslobatristal <ekslobatristal@gmail.com>
2024-02-03 10:34:55 +0800

    membuat halaman footer (1.23.00)

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Shop from './Pages/Shop';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
           <Route path='/chrt' element={<Cart />} />
           <Route path='/login' element={<LoginSignup />} />
         </Routes >
+        <Footer />
       </BrowserRouter >
     </div >
   );
