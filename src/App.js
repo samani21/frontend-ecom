9e5085cd108d220be56af6468be154a3d46fa475
@ekslobatristal <ekslobatristal@gmail.com>
2024-02-03 11:51:51 +0800

    membuat halaman berdasarkan kategori yg dipilih (1.39.00)

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Shop from './Pages/Shop';
 import Footer from './Components/Footer/Footer';
+import men_benner from './Components/Assets/banner_mens.png'
+import women_benner from './Components/Assets/banner_women.png'
+import kid_benner from './Components/Assets/banner_kids.png'
 
 function App() {
   return (
@@ -15,9 +18,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Shop />} />
-          <Route path='/mens' element={< ShopCategory category="men" />} />
-          <Route path='/womens' element={< ShopCategory category="women" />} />
-          <Route path='/kids' element={< ShopCategory category="kid" />} />
+          <Route path='/mens' element={< ShopCategory banner={men_benner} category="men" />} />
+          <Route path='/womens' element={< ShopCategory banner={women_benner} category="women" />} />
+          <Route path='/kids' element={< ShopCategory banner={kid_benner} category="kid" />} />
           <Route>
             <Route path=':productId' element={<Product />} />
           </Route>
